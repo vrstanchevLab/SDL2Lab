5f57ca5b4cbab3c97f176981ca4aabe62a6e10d5
vrstanchev <vrstanchev@gmail.com>
2024-11-04 21:04:46 +0200

    Add GitHub Actions workflow for iOS build

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window *window = SDL_CreateWindow("My iOS App",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          800, 600,
+                                          SDL_WINDOW_SHOWN);
+    if (!window) {
+        fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create a renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "Could not create renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop flag
+    int running = 1;
+    SDL_Event event;
+
+    // Main loop
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 128, 255, 255); // Blue
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    // Cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
